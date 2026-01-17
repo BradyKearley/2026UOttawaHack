eedead87dcb27ec1e23fedd8386d6d723f4430f5
BradyKearley <bradykearley84@gmail.com>
2026-01-17 00:34:34 -0500

    Monster

---
+++ monster.gd
@@ -0,0 +1,32 @@
+extends Node3D
+
+func _on_small_area_area_entered(area: Area3D) -> void:
+	pass # Replace with function body.
+
+
+func _on_small_area_area_exited(area: Area3D) -> void:
+	pass # Replace with function body.
+
+
+func _on_small_area_2_area_entered(area: Area3D) -> void:
+	pass # Replace with function body.
+
+
+func _on_small_area_2_area_exited(area: Area3D) -> void:
+	pass # Replace with function body.
+
+
+func _on_big_area_area_entered(area: Area3D) -> void:
+	pass # Replace with function body.
+
+
+func _on_big_area_area_exited(area: Area3D) -> void:
+	pass # Replace with function body.
+
+
+func _on_big_area_2_area_entered(area: Area3D) -> void:
+	pass # Replace with function body.
+
+
+func _on_big_area_2_area_exited(area: Area3D) -> void:
+	pass # Replace with function body.
